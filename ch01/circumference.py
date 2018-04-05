0cc4bb396358a3256a6f42e1fd991d569b5e1743
yanzhen74 <yanzhen@csu.ac.cn>
2018-04-05 22:49:46 +0800

    fufill pep8

--- ch01/circumference.py
+++ ch01/circumference.py
@@ -12,6 +12,6 @@ radiusInteger = int(radiusString)
 circumference = 2 * math.pi * radiusInteger
 area = math.pi * (radiusInteger ** 2)
 
-print("The cirumference is:",circumference, \
-    ", and the area is:", area)
+print("The cirumference is:", circumference,
+      ", and the area is:", area)
 
