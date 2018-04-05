a1dca07d0a1a48a01111ad5b18fe944fa303e7a3
yanzhen74 <yanzhen@csu.ac.cn>
2018-04-05 22:34:33 +0800

    to python3

--- ch01/circumference.py
+++ ch01/circumference.py
@@ -6,11 +6,12 @@
 
 import math
 
-radiusString = raw_input("Enter the radius of your circle: ")
+radiusString = input("Enter the radius of your circle: ")
 radiusInteger = int(radiusString)
 
 circumference = 2 * math.pi * radiusInteger
 area = math.pi * (radiusInteger ** 2)
 
-print "The cirumference is:",circumference, \
-    ", and the area is:", area
+print("The cirumference is:",circumference, \
+    ", and the area is:", area)
+
