12a15146dd9adda1dcd363da81310ff8c6552d28
yanzhen74 <yanzhen@csu.ac.cn>
2018-04-07 02:04:23 +0800

    add 2 testcase

--- ch01/test_ch01.py
+++ ch01/test_ch01.py
@@ -12,3 +12,5 @@ class TestE26(unittest.TestCase):
 		self.assertEqual(get_record_pos(5, 3, 30), (2, 5, 3))
 		self.assertEqual(get_record_pos(5, 3, 33), (3, 1, 3))
 		self.assertEqual(get_record_pos(5, 3, 31), (3, 1, 1))
+		self.assertEqual(get_record_pos(5, 3, 15), (1, 5, 3))
+		self.assertEqual(get_record_pos(5, 3, 1), (1, 1, 1))
