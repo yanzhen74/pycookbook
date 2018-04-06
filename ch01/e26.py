e5553b1234f436c421fe014782d6d662c89fdcda
yanzhen74 <yanzhen@csu.ac.cn>
2018-04-06 23:40:35 +0800

    e26: cal from base0, simple and easy to understand

--- ch01/e26.py
+++ ch01/e26.py
@@ -1,13 +1,12 @@
-def get_record_pos(n, c, x):
-    npp = n * c
-    _c = x % c
-    p = int(x / npp) + 1
-    p = p - 1 if x % npp == 0 else p
-    _n = int((x - (p - 1) * npp) / c) + 1
-    _n = _n - 1 if _c == 0 else _n
-    _c = c if _c == 0 else _c
-    #print(p, _n, _c, sep='-')
-    return p, _n, _c
+def get_record_pos(nLine, nColum, _nItem):
 
+	_nItemBase0 = _nItem - 1
+	nItemsPerPage = nLine * nColum
+	_nPage = _nItemBase0 // nItemsPerPage + 1
 
-get_record_pos(5, 3, 16)
+	_nLine = (_nItemBase0 % nItemsPerPage) // nColum + 1
+
+	_nColum = _nItemBase0 % nColum + 1
+
+	return _nPage, _nLine, _nColum
+
