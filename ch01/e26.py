22e303e5cc25b8f857471646963af4e28acec0e9
yanzhen74 <yanzhen@csu.ac.cn>
2018-04-06 22:01:20 +0800

    e26 get record pos of page-line-column

---
+++ ch01/e26.py
@@ -0,0 +1,12 @@
+def get_record_pos(n, c, x):
+    npp = n * c
+    _c = x % c
+    p = int(x / npp) + 1
+    p = p - 1 if x % npp == 0 else p
+    _n = int((x - (p - 1) * npp) / c) + 1
+    _n = _n - 1 if _c == 0 else _n
+    _c = c if _c == 0 else _c
+    print(p, _n, _c, sep='-')
+
+
+get_record_pos(5, 3, 16)
